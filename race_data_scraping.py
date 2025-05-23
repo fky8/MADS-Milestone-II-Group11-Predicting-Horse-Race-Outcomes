4f2d02573e022d56831c64500149a28732584a59
KY Fung <45853162+fky8@users.noreply.github.com>
2025-05-23 19:52:52 +0800

    upload race data

--- race_data_scraping.py
+++ race_data_scraping.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 # ─── CONFIG ────────────────────────────────────────────────────────────────
-START_YEAR = 2010
+START_YEAR = 2015
 
 # Selenium:
 chrome_opts = Options()
